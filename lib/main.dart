c581a7d91c2c148990b0bf7b7d40b9afa5d2f1e8
mechellesieglitz <mechelle.sieglitz@gmail.com>
2023-08-24 13:43:00 -0400

    Added adaptive layout

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:character_viewer/pages/character_detail_view/character_detail_vi
 import 'package:character_viewer/pages/character_list_view/character_list_view_cubit.dart';
 import 'package:character_viewer/utils/app_routes.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
+import 'package:sizer/sizer.dart';
 import 'pages/character_list_view/character_list_view.dart';
 import 'package:flutter/material.dart';
 
@@ -17,24 +17,27 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      routes: {
-        AppRoutes.home: (context) =>
-            BlocProvider(
-              create: (context) => CharacterListViewCubit(),
-              child: CharacterListView(),
-            ),
-        AppRoutes.detail: (context) =>
-            BlocProvider(
-              create: (context) => CharacterDetailViewCubit(),
-              child: CharacterDetailView(),
-            ),
-      }
-    );
+    return Sizer(
+        builder: (context, orientation, deviceType) {
+          return MaterialApp(
+              title: 'Flutter Demo',
+              theme: ThemeData(
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                useMaterial3: true,
+              ),
+              routes: {
+                AppRoutes.home: (context) =>
+                    BlocProvider(
+                      create: (context) => CharacterListViewCubit(),
+                      child: CharacterListView(),
+                    ),
+                AppRoutes.detail: (context) =>
+                    BlocProvider(
+                      create: (context) => CharacterDetailViewCubit(),
+                      child: CharacterDetailView(),
+                    ),
+              }
+          );
+        });
   }
 }
--- lib/pages/character_list_view/character_list_view.dart
+++ lib/pages/character_list_view/character_list_view.dart
@@ -4,7 +4,9 @@ import 'package:character_viewer/pages/character_list_view/character_list_view_c
 import 'package:character_viewer/widgets/error_dialog.dart';
 import 'package:character_viewer/widgets/full_screen_loading_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sizer/sizer.dart';
 
 class CharacterListView extends StatefulWidget {
   const CharacterListView({Key? key}) : super(key: key);
@@ -17,6 +19,7 @@ class _CharacterListViewState extends State<CharacterListView> {
   List<Character> characters = [];
   List<Character> filteredList = [];
   String searchField = '';
+  Character? selectedCharacter;
 
   @override
   void initState() {
@@ -38,76 +41,214 @@ class _CharacterListViewState extends State<CharacterListView> {
   @override
   Widget build(BuildContext context) {
     final filteredList = _getFilteredList();
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Character List'),
-        actions: [
-          IconButton(onPressed: () {
-            context.read<CharacterListViewCubit>().getCharacters();
-    }, icon: Icon(Icons.refresh))
-        ],
-      ),
-      body: SafeArea(
-          child: BlocConsumer<CharacterListViewCubit, CharacterListViewState>(
-              listener: (context, state) {
-                if (state is CharacterListViewCharactersReturned) {
-                  setState(() {
-                    characters = state.characters;
-                  });
-                } else if (state is CharacterListViewError) {
-                  showDialog(
-                      context: context,
-                      builder: (dialogContext) {
-                        return ErrorMessageDialog(
-                          title: 'Uh Oh',
-                          message: 'Looks like we couldn\'t find character information. Please try again later.',
-                        );
-                      });
-                }
-              },
-              builder: (context, state) {
-                return Stack(
-                  children: [
-                    Column(
-                      children: [
-                        Container(
-                          height: 60,
-                          child: TextField(
-                            decoration: InputDecoration(
-                                prefixIcon: const Icon(Icons.search),
-                                hintText: 'Search',
-                                border: OutlineInputBorder(),
-                                contentPadding: EdgeInsets.only()),
-                            onChanged: (text) {
+
+    final screenWidth = MediaQuery.of(context).size.width;
+    Orientation currentOrientation = MediaQuery.of(context).orientation;
+    DeviceType deviceType = SizerUtil.deviceType;
+    if (currentOrientation == Orientation.landscape && deviceType == DeviceType.tablet) {
+      return Scaffold(
+          appBar: AppBar(
+            title: Text('Character List'),
+            actions: [
+              IconButton(onPressed: () {
+                context.read<CharacterListViewCubit>().getCharacters();
+              }, icon: Icon(Icons.refresh))
+            ],
+          ),
+        body: Row(
+          children: [
+            // use SizedBox to constrain the AppMenu to a fixed width
+            SizedBox(
+              width: MediaQuery.sizeOf(context).width / 2 - 5,
+              child: Scaffold(
+                  appBar: AppBar(
+                    title: Text('Character List'),
+                    actions: [
+                      IconButton(onPressed: () {
+                        context.read<CharacterListViewCubit>().getCharacters();
+                      }, icon: Icon(Icons.refresh))
+                    ],
+                  ),
+                  body: SafeArea(
+                      child: BlocConsumer<CharacterListViewCubit, CharacterListViewState>(
+                          listener: (context, state) {
+                            if (state is CharacterListViewCharactersReturned) {
                               setState(() {
-                                searchField = text;
+                                characters = state.characters;
                               });
-                            },
+                            } else if (state is CharacterListViewError) {
+                              showDialog(
+                                  context: context,
+                                  builder: (dialogContext) {
+                                    return ErrorMessageDialog(
+                                      title: 'Uh Oh',
+                                      message: 'Looks like we couldn\'t find character information. Please try again later.',
+                                    );
+                                  });
+                            }
+                          },
+                          builder: (context, state) {
+                            return Stack(
+                                children: [
+                                  Column(
+                                    children: [
+                                      Container(
+                                        height: 60,
+                                        child: TextField(
+                                          decoration: InputDecoration(
+                                              prefixIcon: const Icon(Icons.search),
+                                              hintText: 'Search',
+                                              border: OutlineInputBorder(),
+                                              contentPadding: EdgeInsets.only()),
+                                          onChanged: (text) {
+                                            setState(() {
+                                              searchField = text;
+                                            });
+                                          },
+                                        ),
+                                      ),
+                                      Expanded(
+                                          child: ListView.builder(
+                                              itemCount: filteredList.length,
+                                              itemBuilder: (BuildContext context, int index) {
+                                                if (filteredList.length < 0) {
+                                                  return ListTile(
+                                                      title: Text('No Characters Returned...Yet')
+                                                  );
+                                                } else {
+                                                  Character character = filteredList.elementAt(index);
+                                                  return GestureDetector(
+                                                      onTap: () {
+                                                        setState(() {
+                                                          selectedCharacter = character;
+                                                        });
+                                                      },
+                                                      child: ListTile(
+                                                        title: Text(character.name ?? 'Error'),
+                                                      ));
+                                                }
+                                              })
+                                      ),
+                                    ],
+                                  ),
+                                  Visibility(child: FullScreenLoadingWidget(),visible: state is CharacterListViewLoading,)
+                                ]);
+                          }))),
+            ),
+            // vertical black line as separator
+            Container(width: 0.5, color: Colors.black),
+            // use Expanded to take up the remaining horizontal space
+            Expanded(
+              child: selectedCharacter != null ? Scaffold(
+                  appBar: AppBar(
+                    title: Text(selectedCharacter?.name ?? ''),
+                  ),
+                  body: SafeArea(
+                      child: Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: Column(
+                                  children: [
+                                    Center(
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: selectedCharacter != null ? Image.network(selectedCharacter?.iconURL ?? '', fit: BoxFit.cover, width: MediaQuery.sizeOf(context).width/2,) : Container(),
+                                      ),
+                                    ),
+                                    Padding(padding: EdgeInsets.symmetric(vertical: 20.0), child: Text(selectedCharacter?.text ?? ''),)
+                                  ],
+                                ),
+                              )
+                  )
+              )
+                  : Scaffold(
+                appBar: AppBar(
+                  title: Text('Select a Character'),
+                ),
+                body: Text('Please select a character to continue'),
+              ) )
+          ])
+      );
+
+    } else {
+      return Scaffold(
+          appBar: AppBar(
+            title: Text('Character List'),
+            actions: [
+              IconButton(onPressed: () {
+                context.read<CharacterListViewCubit>().getCharacters();
+              }, icon: Icon(Icons.refresh))
+            ],
+          ),
+          body: SafeArea(
+              child: BlocConsumer<CharacterListViewCubit, CharacterListViewState>(
+                  listener: (context, state) {
+                    if (state is CharacterListViewCharactersReturned) {
+                      setState(() {
+                        characters = state.characters;
+                      });
+                    } else if (state is CharacterListViewError) {
+                      showDialog(
+                          context: context,
+                          builder: (dialogContext) {
+                            return ErrorMessageDialog(
+                              title: 'Uh Oh',
+                              message: 'Looks like we couldn\'t find character information. Please try again later.',
+                            );
+                          });
+                    }
+                  },
+                  builder: (context, state) {
+                    return Stack(
+                        children: [
+                          Column(
+                            children: [
+                              Container(
+                                height: 60,
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                      prefixIcon: const Icon(Icons.search),
+                                      hintText: 'Search',
+                                      border: OutlineInputBorder(),
+                                      contentPadding: EdgeInsets.only()),
+                                  onChanged: (text) {
+                                    setState(() {
+                                      searchField = text;
+                                    });
+                                  },
+                                ),
+                              ),
+                              Expanded(
+                                  child: ListView.builder(
+                                      itemCount: filteredList.length,
+                                      itemBuilder: (BuildContext context, int index) {
+                                        if (filteredList.length < 0) {
+                                          return ListTile(
+                                              title: Text('No Characters Returned...Yet')
+                                          );
+                                        } else {
+                                          Character character = filteredList.elementAt(index);
+                                          return GestureDetector(
+                                              onTap: () {
+                                                CharacterDetailView.routeTo(context, character);
+                                              },
+                                              child: ListTile(
+                                                title: Text(character.name ?? 'Error'),
+                                              ));
+                                        }
+                                      })
+                              ),
+                            ],
                           ),
-                        ),
-                        Expanded(
-                          child: ListView.builder(
-                              itemCount: filteredList.length,
-                              itemBuilder: (BuildContext context, int index) {
-                                if (filteredList.length < 0) {
-                                  return ListTile(
-                                      title: Text('No Characters Returned...Yet')
-                                  );
-                                } else {
-                                  Character character = filteredList.elementAt(index);
-                                  return GestureDetector(
-                                      onTap: () {
-                                        CharacterDetailView.routeTo(context, character);
-                                      },
-                                      child: ListTile(
-                                        title: Text(character.name ?? 'Error'),
-                                      ));
-                                }}),
-                        ),
-                      ],
-                    ),
-                    Visibility(child: FullScreenLoadingWidget(),visible: state is CharacterListViewLoading,)
-                ]);
-              })));
+                          Visibility(child: FullScreenLoadingWidget(),visible: state is CharacterListViewLoading,)
+                        ]);
+                  })));
+    }
+
+
+
   }
+
+  // ListView getListView() {
+  //   return
+  // }
 }
